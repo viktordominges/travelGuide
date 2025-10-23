c58c686141eeee6801bf7b8baae8f5e5e28cc171
Viktor Dominhes <viktordominges@gmail.com>
2025-10-23 18:17:31 +0200

    Implement accounts app with custom user model and registration serializer

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации пользователя"""
+    password = serializers.CharField(
+        write_only=True, 
+        validators=[validate_password]
+    )
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'password', 'password_confirm',
+            'first_name', 'last_name'
+        )
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didnt match."}
+            )
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
